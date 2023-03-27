16e10b4a178b0073eb64876f6dabae3b284d93d7
Jason <jazzmoret@gmail.com>
2023-03-27 15:10:33 +0200

    test Tokens

--- Festivals.swiftpm/Utils/RequestExtension.swift
+++ Festivals.swiftpm/Utils/RequestExtension.swift
@@ -29,6 +29,7 @@ extension URLSession {
         var request :URLRequest = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(UserDefaults.standard.string(forKey: "token"), forHTTPHeaderField: "token")
         do {
             let (data,response) = try await upload(for: request, from: encoded, delegate: nil)
             let httpResponse = response as! HTTPURLResponse
@@ -54,6 +55,7 @@ extension URLSession {
         var request :URLRequest = URLRequest(url: url)
         request.httpMethod = "PUT"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(UserDefaults.standard.string(forKey: "token"), forHTTPHeaderField: "token")
         do {
             let (_,response) = try await URLSession.shared.upload(for: request, from: encoded, delegate: nil)
             let httpResponse = response as! HTTPURLResponse
@@ -76,6 +78,7 @@ extension URLSession {
         var request :URLRequest = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(UserDefaults.standard.string(forKey: "token"), forHTTPHeaderField: "token")
         do{
             let (_,response) = try await upload(for: request, from: encoded, delegate: nil)
             let httpResponse = response as! HTTPURLResponse
