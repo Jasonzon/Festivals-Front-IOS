1ff2c7f8fff2ec65ba9e4197d4f655ca6a449938
Jason <jazzmoret@gmail.com>
2023-04-02 19:17:26 +0200

    update affichage

--- Festivals.swiftpm/View/FestivalView.swift
+++ Festivals.swiftpm/View/FestivalView.swift
@@ -10,7 +10,6 @@ struct FestivalView: View {
     @State private var showingAlertNotDismiss = false
     @State private var errorAlert = false
     @State private var textAlert = ""
-    @State private var showJours = false
     
     init(festival: Festival, festivalsViewModel: FestivalsViewModel){
         self.festivalViewModel = FestivalViewModel(model: festival)
@@ -21,72 +20,63 @@ struct FestivalView: View {
     }
 
     var body: some View {
-        VStack {
-            if !showJours {
-                if (UserSession.shared.user?.role == .Admin) {
-                    Form {
-                        TextField("Nom", text: $festivalViewModel.name)
-                        TextField("Année", text: $festivalViewModel.year)
-                        Picker(selection: $festivalViewModel.opened, label: Text("Statut")) {
-                            Text("Ouvert").tag(true)
-                            Text("Fermé").tag(false)
-                        }
-                        Section {
-                            Button("Enregistrer") {
-                                Task {
-                                    intent.intentTestValidation(festival: festivalViewModel.getFestivalFromViewModel())
-                                    if festivalViewModel.error == .noError {
-                                        let data = await intent.intentValidation(festival: festivalViewModel.copyModel)
-                                        switch data {
-                                            case .success(_):
-                                                showingAlert = true
-                                                textAlert = "Festival mis à jour"
-                                            case .failure(let err):
-                                                errorAlert = true
-                                                textAlert = "Erreur \(err)"
-                                        }
-                                    }
-                                }
-                            }
-                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-                            Button("Supprimer") {
-                                Task {
-                                    let data = await intent.intentDeleteRequest(id: festivalViewModel.id)
+        ScrollView {
+        VStack(spacing: 20) {
+            if (UserSession.shared.user?.role == .Admin) {
+                Form {
+                    TextField("Nom", text: $festivalViewModel.name)
+                    TextField("Année", text: $festivalViewModel.year)
+                    Picker(selection: $festivalViewModel.opened, label: Text("Statut")) {
+                        Text("Ouvert").tag(true)
+                        Text("Fermé").tag(false)
+                    }
+                    Section {
+                        Button("Enregistrer") {
+                            Task {
+                                intent.intentTestValidation(festival: festivalViewModel.getFestivalFromViewModel())
+                                if festivalViewModel.error == .noError {
+                                    let data = await intent.intentValidation(festival: festivalViewModel.copyModel)
                                     switch data {
                                         case .success(_):
-                                            break
+                                            showingAlert = true
+                                            textAlert = "Festival mis à jour"
                                         case .failure(let err):
                                             errorAlert = true
-                                            textAlert = "Erreur \(err)" 
+                                            textAlert = "Erreur \(err)"
                                     }
                                 }
                             }
-                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center).foregroundColor(.red)
                         }
-                        .onChange(of: festivalViewModel.error) { error in
-                            print(error)
-                            if (error != .noError) {
-                                textAlert = "\(error)"
-                                errorAlert = true
-                            }   
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                        Button("Supprimer") {
+                            Task {
+                                let data = await intent.intentDeleteRequest(id: festivalViewModel.id)
+                                switch data {
+                                    case .success(_):
+                                        break
+                                    case .failure(let err):
+                                        errorAlert = true
+                                        textAlert = "Erreur \(err)" 
+                                }
+                            }
                         }
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center).foregroundColor(.red)
                     }
-                }
-                else {
-                    Text("Nom : \(festivalViewModel.name)")
-                    Text("Année : \(festivalViewModel.year)")
-                    Text("Statut : \(festivalViewModel.opened ? "Ouvert" : "Fermé")")
-                }
-                Button("Afficher les jours") {
-                    showJours = true
+                    .onChange(of: festivalViewModel.error) { error in
+                        print(error)
+                        if (error != .noError) {
+                            textAlert = "\(error)"
+                            errorAlert = true
+                        }   
+                    }
                 }
             }
             else {
-                Button("Cacher les jours") {
-                    showJours = false
-                }
-                JoursView(festival: $festivalViewModel.copyModel.id, year: $festivalViewModel.copyModel.year)
+                Text("Nom : \(festivalViewModel.name)")
+                Text("Année : \(festivalViewModel.year)")
+                Text("Statut : \(festivalViewModel.opened ? "Ouvert" : "Fermé")")
             }
+            JoursView(festival: $festivalViewModel.copyModel.id, year: $festivalViewModel.copyModel.year)
         }
         .toast(isPresenting: $showingAlert, alert: {
             AlertToast(displayMode: .hud, type: .complete(.green), title: textAlert)
@@ -99,5 +89,6 @@ struct FestivalView: View {
             festivalViewModel.error = .noError
             errorAlert = false
         })   
+        }
     }
 }
